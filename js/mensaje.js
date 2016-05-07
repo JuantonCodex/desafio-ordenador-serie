d45bd7c251a086146a824c6439c9700437ca4771
Juan Cahuana <jcahuana@attachmedia.com>
2016-05-07 18:35:42 -0500

    Primer presentable del ordenador terminado

--- js/mensaje.js
+++ js/mensaje.js
@@ -1,13 +1,22 @@
 var mensaje = (function(){
   var dom = {};
-  dom.msg = $('.Mensaje');
+  dom.msg      = $('.Mensaje');
   dom.msg_text = $('.Mensaje-content .text');
 
   var init = function(){
     ocultar();
   };
 
-  var mostrar = function(texto){
+  var mostrar = function(texto, simbolo){
+
+    // Mostra ícono
+    if (simbolo === 'alert') {
+      dom.msg.find('.icon.alert').addClass('is-visible');
+    }else if(simbolo === 'success'){
+      dom.msg.find('.icon.success').addClass('is-visible');
+    }
+
+    // Insertar texto
     dom.msg_text.text(texto);
     dom.msg.addClass('is-visible');
 
@@ -16,6 +25,7 @@ var mensaje = (function(){
   var ocultar = function(){
     $('body').on('click', '.Mensaje-overlay', function(){
       dom.msg.removeClass('is-visible');
+      dom.msg.find('.icon').removeClass('is-visible');
     });
   };
 
